02425f393f2888b7e10c7af89dc5dfb6d4af67cf
Stephan Soller <stephan.soller@helionweb.de>
2012-04-05 18:07:24 +0200

    Added buildins to the printer.

--- printer.c
+++ printer.c
@@ -6,22 +6,35 @@
 void print_list(FILE *stream, atom_t *list_atom);
 
 void print_atom(FILE *stream, atom_t *atom){
-	if (atom->type == T_NUM)
-		fprintf(stream, "%ld", atom->num);
-	else if (atom->type == T_SYM)
-		fprintf(stream, "%s", atom->sym);
-	else if (atom->type == T_STR)
-		fprintf(stream, "\"%s\"", atom->str);
-	else if (atom->type == T_NIL)
-		fprintf(stream, "nil");
-	else if (atom->type == T_TRUE)
-		fprintf(stream, "true");
-	else if (atom->type == T_FALSE)
-		fprintf(stream, "false");
-	else if (atom->type == T_PAIR)
-		print_list(stream, atom);
-	else
-		fprintf(stream, "unknown atom");
+	switch(atom->type){
+		case T_NUM:
+			fprintf(stream, "%ld", atom->num);
+			break;
+		case T_SYM:
+			fprintf(stream, "%s", atom->sym);
+			break;
+		case T_STR:
+			fprintf(stream, "\"%s\"", atom->str);
+			break;
+		case T_NIL:
+			fprintf(stream, "nil");
+			break;
+		case T_TRUE:
+			fprintf(stream, "true");
+			break;
+		case T_FALSE:
+			fprintf(stream, "false");
+			break;
+		case T_PAIR:
+			print_list(stream, atom);
+			break;
+		case T_BUILDIN:
+			fprintf(stream, "buildin at %p", atom->func);
+			break;
+		default:
+			fprintf(stream, "unknown atom");
+			break;
+	}
 }
 
 void print_list(FILE *stream, atom_t *list_atom){
